41cbb5d760e030fa3f6824f3ffa0651582ea1f8c
Ayan <sujon.seo.bd@gmail.com>
2025-08-20 13:32:20 +0600

    added some code

--- module-25/js/handler.js
+++ module-25/js/handler.js
@@ -1,1 +1,17 @@
-console.log()
+        document.getElementById('btn-update-title')
+        .addEventListener('click', function(){
+            // console.log('button Clicked');
+            const pageTitleElement = document.getElementById('title-text');
+            // console.log(pageTitleElement);
+            pageTitleElement.innerText = "Updated page title text";
+        })
+        //  another button
+        document.getElementById('button-login')
+        .addEventListener('click', function(){
+            // console.log('login button clicked');
+            const userInfoEl = document.getElementById('user-info');
+            userInfoEl.innerText = "User logged in successfully."
+            
+        })
+
+        // another example
